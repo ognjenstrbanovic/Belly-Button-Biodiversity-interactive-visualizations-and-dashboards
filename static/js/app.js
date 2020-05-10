36c2e609caac48075a5eead1952d923d6c6e422e
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-05-10 13:36:30 -0400

    first commit

---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+d3.json("data/samples.json").then((data) => {
+    console.log(data);
+    // data.samples.forEach(Top10);
+    // function Top10() {
+    //     for (i = 0; i < 10; i++) {
+    //         Math.max.apply();
+    //     };
+    // }
+});
+
